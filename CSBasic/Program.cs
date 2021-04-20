4979df7922226493d678f01c88d58fb7c96ce18c
YeonHui Kim <hie0908@naver.com>
2021-04-20 12:03:14 +0900

    #2 Console.WriteLine()을 써서 출력함

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -36,7 +36,12 @@ namespace CSBasic
              * comment
              */
 
-            Console.WriteLine("Hello");
+            // 출력
+            Console.Write("이건 안개행");
+            Console.Write("입니다\n");
+            Console.WriteLine("");
+            Console.WriteLine("이건 개행");
+
         }
     }
 }
