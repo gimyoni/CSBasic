b78421feec2f45e07eae2e2f49a4ffd58148b646
YeonHui Kim <hie0908@naver.com>
2021-05-06 13:23:40 +0900

    #18 while / do while 실습 [가-힣]

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -13,6 +13,23 @@ namespace CSBasic3
             // Console.WriteLine(intArray[100]); // 범위 추가
             // Console.WriteLine(intArray[-]); // 음수 사용 불가
 
+            int i = 0;
+            int[] intArray2 = { 52, 273, 32, 65, 103 };
+            while (i < intArray.Length)
+            {
+                Console.WriteLine(i + "번째 출력" + intArray2[i]);
+                i++;
+            }
+            string input;
+            do
+            {
+                Console.Write("입력(종료:exit):");
+                input = Console.ReadLine();
+            } while (input != "exit");
+            for (int j = '가'; j <= '힣'; j++)
+            {
+                Console.WriteLine((char)j);
+            }
         }
     }
 }
