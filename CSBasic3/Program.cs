fac20d341dc002ea1308e6e233525640cd0c0c28
YeonHui Kim <hie0908@naver.com>
2021-05-13 10:23:14 +0900

    #20 반복문 응용 - 대소문자 변환, path, Thread.Sleep, SetCursorPosition

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace CSBasic3
 {
@@ -67,6 +68,73 @@ namespace CSBasic3
                     Console.Write('*');
                 }
                 Console.WriteLine();
+            }
+
+            // 대소문자 변환
+            string input3 = "Potato Tomato";
+            Console.WriteLine(input3.ToUpper());
+            Console.WriteLine(input3.ToLower());
+            input3.ToLower(); // 헛고생
+            Console.WriteLine(input); // 기존 그대로 출력됨
+            string foods = "감자 고구마 토마토";
+            string[] foodsArray = foods.Split(new char[] { ' ' });
+            foreach (var item in foodsArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            // 환경변수의 path 텍스트 값을 가져와서 \를 \\로 치환해서 넣어준다
+            string path = "C:\\oraclexe\\app\\oracle\\product\\11.2.0\\server\\bin;C:\\Program Files (x86)\\Common Files\\Oracle\\Java\\javapath;%SystemRoot%\\system32;%SystemRoot%;%SystemRoot%\\System32\\Wbem;%SYSTEMROOT%\\System32\\WindowsPowerShell\\v1.0\\;%SYSTEMROOT%\\System32\\OpenSSH\\;%JAVA_HOME%\\bin;C:\\Program Files\\dotnet\\;C:\\Program Files\\Microsoft SQL Server\\130\\Tools\\Binn\\;C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\170\\Tools\\Binn\\;C:\\Program Files\\Git\\cmd;C:\\Program Files\\nodejs\\;C:\\Program Files\\MongoDB\\Server\\4.4\\bin;C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin;";
+            string[] paths = path.Split(new char[] { ';' });
+            foreach (var item in paths)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            string dirtyInput = " text    um \n\t";
+            Console.WriteLine("[" + dirtyInput + "]");
+            Console.WriteLine("[" + dirtyInput.Trim() + "]");
+            string[] foodsArray2 = { "감자", "고구마", "토마토", "가지" };
+            Console.WriteLine(string.Join(" ", foodsArray2)); // space 1개
+            Console.WriteLine(string.Join(",", foodsArray2));
+            Console.WriteLine(string.Join(";", foodsArray2));
+            Console.WriteLine(string.Join(" ", foodsArray2)); // tab 1개
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("[     ]");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("[#    ]");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("[##   ]");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("[###  ]");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("[#### ]");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("[#####]");
+            Thread.Sleep(1000);
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine("__@");
+                }
+                else if (x % 3 == 1)
+                {
+                    Console.WriteLine("_^@");
+                }
+                else if (x % 3 == 2)
+                {
+                    Console.WriteLine("^_@");
+                }
             }
         }
     }
