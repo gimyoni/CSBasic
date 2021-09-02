194653d5d10b83a4dd571fd717941596d9f8101e
gimyoni <hie0908@naver.com>
2021-09-02 09:45:27 +0900

    IDisposable 인터페이스 예제

--- CSManage8/Program.cs
+++ CSManage8/Program.cs
@@ -1,43 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSBasic8
+﻿namespace CSBasic8
 {
-    class Product : IComparable
-    {
-        public string Name { get; set; }
-        public int Price { get; set; }
-
-        public int CompareTo(object obj)
-        {
-            return Price.CompareTo((obj as Product).Price);
-        }
-
-        public override string ToString()
-        {
-            return Name + " : " + Price + "원";
-        }
-    }
     class Program
     {
+        class Dummy : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("리소스가 해제되었습니다.");
+            }
+        }
+        class Product : IComparable
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                return Price.CompareTo((obj as Product).Price);
+            }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+        }
         static void Main(string[] args)
         {
+            // IComparable
             List<Product> list = new List<Product>()
             {
                   new Product() { Name="고구마", Price=1500},
-                  new Product() { Name="사과", Price=2400},
-                  new Product() { Name="바나나", Price=1000},
-                  new Product() { Name="배", Price=3000},
-                  new Product() { Name="멜론", Price=15000},
-            };
-            list.Sort();
-            //list.Reverse();
-            foreach (var item in list)
+	@@ -39,6 +47,14 @@ static void Main(string[] args)
             {
                 Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            // IDisposable
+            using (Dummy d = new Dummy())
+            {
+                Console.WriteLine("뭔가 했습니다.");
             }
         }
     }
